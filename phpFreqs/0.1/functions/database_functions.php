50035915f2b233ea26919b6af3b9b8cebc046f9d
hepatus <hepatus@a370a7ce-431f-0410-b7e2-c32ad1df68bf>
2006-10-21 21:16:17 +0000

    Basic Database Design Completed, Create initial Database and empty tables functional.

--- phpFreqs/0.1/functions/database_functions.php
+++ phpFreqs/0.1/functions/database_functions.php
@@ -65,14 +65,13 @@ function dbcheck()
 function dbinstalled($db)
 {
   //Verify if system is installed
-  $sql='select version from installed;';
+  $sql='select version from options;';
   $check=@mysql_query($sql,$db);
   
   if (!$check)
   {
     if (mysql_errno() == "1146")
     {
-      echo "Table Doesn't Exist!<br>";
       return(false);
     }
     else
@@ -89,8 +88,41 @@ function dbinstalled($db)
 function doinstall($db)
 {
   //Create tables in DB
+  $users='CREATE TABLE users (id int, username varchar(15), first varchar(25), email varchar(255), password varchar(16), active BOOL, verified BOOL, verifycode varchar(75), location int, lat float, lon float, gmt int signed)';
+  $options='CREATE TABLE options (version int, install_date int, gmt int signed, path varchar(255), name varchar(255), tagline varchar(255))';
+  $freq='CREATE TABLE freq (id int, freq int, category int, lat float, lon float, verified BOOL, protocol int, location int)';
+  $tags='CREATE TABLE tags (user int, freq int, tags text)';
+  $cat='CREATE TABLE category(id int, decription varchar(50), abbrev varchar(10))';
+  $com_freq='CREATE TABLE comments_freq (user int, freq int, date int, comment text)';
+  $com_loc='CREATE TABLE comments_loc (user int, location int, date int, comment text)';
+  $country='CREATE TABLE country (id int, name varchar(75))';
+  $state='CREATE TABLE state (id int, name varchar(75), country int)';
+  $city='CREATE TABLE city (id int, name varchar(75), state int)';
   
+  install_create_tables($users, $db);
+  install_create_tables($options, $db);
+  install_create_tables($freq, $db);
+  install_create_tables($tags, $db);
+  install_create_tables($cat, $db);
+  install_create_tables($com_freq, $db);
+  install_create_tables($com_loc, $db);
+  install_create_tables($country, $db);
+  install_create_tables($state, $db);
+  install_create_tables($city, $db);
+  //Not sure if there is a more effecient way to do above, but it works ;)
+
+  //If we are still alive, then installation must have been successful
+  return(true);
+
 }
 
+function install_create_tables($sql, $db)
+{
+  $result=mysql_query($sql,$db);
+  if (!$result)
+  {
+    die('Oops.. something went wrong '.mysql_error().'<br>');
+  }
+}
 	
 ?>
